139d04a8d47aaa17e1a6d21870ffceed5d0f28a2
qiewi <105619444+qiewi@users.noreply.github.com>
2024-11-03 16:25:07 +0700

    objective function udah oke tapi diagonal bidangnya masih bug

--- models/cube.py
+++ models/cube.py
@@ -1,55 +1,71 @@
+import numpy as np
+import random
+
 class Cube:
     def __init__(self):
-        # Inisialisasi kubus 5x5x5 dengan angka dari 1 hingga 125
         self.n = 5
-        self.magic_number = self.calculate_magic_number()
-        self.cube = [[[num + 1 + x * self.n**2 + y * self.n for num in range(self.n)] 
-                      for y in range(self.n)] 
-                     for x in range(self.n)]
-    
-    def calculate_magic_number(self):
-        # Menghitung magic number untuk kubus 5x5x5
-        return (self.n * (self.n**3 + 1)) // 2
-    
+        self.magic_number = 315
+
+        # Mengisi kubus 5x5x5 dengan angka acak dari 1 hingga 125
+        numbers = list(range(1, self.n ** 3 + 1))
+        random.shuffle(numbers)
+        self.cube = np.array([[[numbers.pop() for _ in range(self.n)] for _ in range(self.n)] for _ in range(self.n)])
+
+
     def objective_function(self):
-        # Jumlah dari selisih absolut dari magic number untuk setiap aturan
-        f_s = 0
-        f_s += sum(abs(self.magic_number - self.sum_row(i, j)) for i in range(self.n) for j in range(self.n))
-        f_s += sum(abs(self.magic_number - self.sum_column(i, j)) for i in range(self.n) for j in range(self.n))
-        f_s += sum(abs(self.magic_number - self.sum_pillar(i, j)) for i in range(self.n) for j in range(self.n))
-        f_s += sum(abs(self.magic_number - self.sum_diagonal(i)) for i in range(4)) # Ada empat diagonal 3D
-        f_s += sum(abs(self.magic_number - self.sum_slice(i, j)) for i in range(self.n) for j in range(3))
-        return f_s
+        # Mengambil magic number
+        magic_number = self.magic_number
+        total_difference = 0
 
-    def sum_row(self, layer, row):
-        # Jumlah elemen pada baris tertentu di satu lapisan 5x5
-        return sum(self.cube[layer][row][col] for col in range(self.n))
+        # Perbedaan jumlah di setiap baris
+        for layer in self.cube:
+            for row in layer:
+                row_sum = np.sum(row)
+                total_difference += abs(magic_number - row_sum)
 
-    def sum_column(self, layer, col):
-        # Jumlah elemen pada kolom tertentu di satu lapisan 5x5
-        return sum(self.cube[layer][row][col] for row in range(self.n))
+        # Perbedaan jumlah di setiap kolom
+        for layer in self.cube:
+            for col in range(self.n):
+                col_sum = np.sum(layer[:, col])
+                total_difference += abs(magic_number - col_sum)
 
-    def sum_pillar(self, row, col):
-        # Jumlah elemen pada tiang tertentu di semua lapisan
-        return sum(self.cube[layer][row][col] for layer in range(self.n))
+        # Perbedaan jumlah di setiap tiang (melintasi layer)
+        for row in range(self.n):
+            for col in range(self.n):
+                pillar_sum = np.sum(self.cube[:, row, col])
+                total_difference += abs(magic_number - pillar_sum)
 
-    def sum_diagonal(self, diag_type):
-        # Jumlah elemen pada diagonal ruang utama berdasarkan diag_type
-        # diag_type: 0-3, masing-masing mewakili salah satu dari empat diagonal 3D
-        if diag_type == 0:
-            return sum(self.cube[i][i][i] for i in range(self.n))
-        elif diag_type == 1:
-            return sum(self.cube[i][i][self.n - 1 - i] for i in range(self.n))
-        elif diag_type == 2:
-            return sum(self.cube[i][self.n - 1 - i][i] for i in range(self.n))
-        elif diag_type == 3:
-            return sum(self.cube[self.n - 1 - i][i][i] for i in range(self.n))
+        # Perbedaan jumlah di setiap diagonal ruang
+        diagonals = [
+            np.sum([self.cube[i, i, i] for i in range(self.n)]),
+            np.sum([self.cube[i, i, self.n - i - 1] for i in range(self.n)]),
+            np.sum([self.cube[i, self.n - i - 1, i] for i in range(self.n)]),
+            np.sum([self.cube[i, self.n - i - 1, self.n - i - 1] for i in range(self.n)]),
+        ]
+        total_difference += sum(abs(magic_number - diag_sum) for diag_sum in diagonals)
 
-    def sum_slice(self, axis, index):
-        # Jumlah elemen pada potongan (irisan 2D) dari kubus
-        if axis == 0:  # Irisan sumbu x (lapisan tetap)
-            return sum(self.cube[index][i][j] for i in range(self.n) for j in range(self.n))
-        elif axis == 1:  # Irisan sumbu y (baris tetap)
-            return sum(self.cube[i][index][j] for i in range(self.n) for j in range(self.n))
-        elif axis == 2:  # Irisan sumbu z (kolom tetap)
-            return sum(self.cube[i][j][index] for i in range(self.n) for j in range(self.n))
+        # Perbedaan jumlah di setiap diagonal bidang
+        for layer in self.cube:
+            diagonal1 = np.sum([layer[i, i] for i in range(self.n)])
+            diagonal2 = np.sum([layer[i, self.n - i - 1] for i in range(self.n)])
+            total_difference += abs(magic_number - diagonal1)
+            total_difference += abs(magic_number - diagonal2)
+
+        for row in range(self.n):
+            diagonal1 = np.sum([self.cube[row, i, i] for i in range(self.n)])
+            diagonal2 = np.sum([self.cube[row, i, self.n - i - 1] for i in range(self.n)])
+            total_difference += abs(magic_number - diagonal1)
+            total_difference += abs(magic_number - diagonal2)
+
+        for col in range(self.n):
+            diagonal1 = np.sum([self.cube[i, i, col] for i in range(self.n)])
+            diagonal2 = np.sum([self.cube[i, self.n - i - 1, col] for i in range(self.n)])
+            total_difference += abs(magic_number - diagonal1)
+            total_difference += abs(magic_number - diagonal2)
+
+        return total_difference
+
+# Contoh penggunaan
+cube_instance = Cube()
+objective_value = cube_instance.objective_function()
+print("Nilai fungsi objektif:", objective_value)
