8e12190bb6fca261687f53c98627b2e0da09c1d2
qiewi <105619444+qiewi@users.noreply.github.com>
2024-11-11 18:51:12 +0700

    crossover fixed

--- algorithm/genetic_algorithm.py
+++ algorithm/genetic_algorithm.py
@@ -1,4 +1,6 @@
 # Import library yang dibutuhkan
+from collections import Counter
+from copy import deepcopy
 import numpy as np
 import random
 import Visual
@@ -12,14 +14,12 @@ class GeneticAlgorithm:
         self.best_cube = Cube()
         self.best_value = float('inf')
         self.display_text = []
-        self.mutation_rate = 0.05  
     '''-------------------- Konstruktor Kelas --------------------'''
     
     '''-------------------- Inisiasi Populasi Genetik --------------------'''
     # Inisialisasi populasi
     def populate(self, population_size):
-        population = [Cube() for _ in range(population_size-1)]
-        population.append(self.best_cube)
+        population = [Cube() for _ in range(population_size)]
         return population
     '''-------------------- Inisiasi Populasi Genetik --------------------'''
 
@@ -48,7 +48,7 @@ class GeneticAlgorithm:
     '''-------------------- Selection Process --------------------'''
 
     '''-------------------- Cross-Over Process --------------------'''
-    # Proses Crossover
+    # Crossover function ensuring unique elements in parentren
     def crossover(self, parents):
         new_population = []
 
@@ -58,73 +58,88 @@ class GeneticAlgorithm:
             parent2 = parents[i + 1]
 
             # Menentukan titik crossover secara acak
-            crossover_point = random.randint(1, 126)
+            crossover_point = random.randint(0, 124)
 
-            # Melakukan swap pada parents
-            parent1.cube.flat[crossover_point:], parent2.cube.flat[crossover_point:] = parent2.cube.flat[crossover_point:], parent1.cube.flat[crossover_point:]
+            # Membuat dua kubus baru sebagai child
+            child1 = Cube()
+            child2 = Cube()
+
+            # Melakukan crossover pada titik yang telah ditentukan
+            child1.cube.flat[crossover_point:], child2.cube.flat[crossover_point:] = parent2.cube.flat[crossover_point:], parent1.cube.flat[crossover_point:]
+
+            # Mengisi sisa elemen yang belum terisi pada child1 dan memastikan elemen yang diisi unik
+            currentIndex = 0
+            for j in range(crossover_point, 125):
+                for k in range(currentIndex, 125):
+                    if parent2.cube.flat[k] not in child1.cube.flat:
+                        child1.cube.flat[j] = parent2.cube.flat[k]
+                        currentIdx = k
+                        break
+            
+            # Mengisi sisa elemen yang belum terisi pada child2 dan memastikan elemen yang diisi unik
+            currentIndex = 0
+            for j in range(crossover_point, 125):
+                for k in range(currentIndex, 125):
+                    if parent1.cube.flat[k] not in child2.cube.flat:
+                        child2.cube.flat[j] = parent1.cube.flat[k]
+                        currentIdx = k
+                        break
 
             # Menambahkan parents yang telah di-cross-over ke populasi baru
             new_population.append(parent1)
             new_population.append(parent2)
-        
-        # Mengembalikan populasi baru yang telah di-cross-over
+
         return new_population
     '''-------------------- Cross-Over Process --------------------'''
 
     '''-------------------- Mutation Process --------------------'''
     # Proses Mutasi
     def mutation(self, new_population):
-        # Melakukan mutasi pada populasi baru pada indeks acak
         for individual in new_population:
-            if random.random() < self.mutation_rate:
-                # Menentukan dua indeks acak
-                idx1, idx2 = random.sample(range(len(individual.cube.flat)), 2)
-                # Melakukan swap pada dua indeks tersebut
-                individual.cube.flat[idx1], individual.cube.flat[idx2] = individual.cube.flat[idx2], individual.cube.flat[idx1]
-            
-        # Mengembalikan populasi baru yang telah dimutasi
+            # Melakukan swap pada dua elemen secara acak
+            idx1, idx2 = random.randint(0, 124), random.randint(0, 124)
+            individual.cube.flat[idx1], individual.cube.flat[idx2] = individual.cube.flat[idx2], individual.cube.flat[idx1]
+
         return new_population
     '''-------------------- Mutation Process --------------------'''
 
     '''-------------------- Fungsi Utama Genetic Algorithm --------------------'''
     # Proses pencarian menggunakan algoritma genetik
     def genetic_search(self, population_size, max_iterations):    
-
-        # Inisialisasi list untuk menyimpan nilai objektif terbaik dan rata-rata
+        # Inisialisasi array skor
         min_scores = []
         avg_scores = []
 
+        # Inisialisasi populasi
+        population = self.populate(population_size)
+
         for iteration in range(max_iterations):
-
-            # Inisialisasi populasi
-            population = self.populate(population_size)
-
-            # Proses Seleksi
+            # Proses seleksi
             parents = self.selection(population)
             
-            # Proses Crossover
+            # Proses crossover
             crossover_result = self.crossover(parents)
 
-            # Proses Mutasi
-            new_population = self.mutation(crossover_result)
+            # Proses mutasi
+            population = self.mutation(crossover_result)
 
-            # Menghitung nilai objektif untuk setiap kubus dalam populasi baru
-            scores = [cube.objective_function() for cube in new_population]
+            # Menghitung nilai objektif untuk setiap kubus dalam populasi
+            scores = [cube.objective_function() for cube in population]
+            min_score = min(scores)
+            max_score = max(scores)
 
-            # Menyimpan nilai objektif terbaik dan rata-rata
-            min_scores.append(min(scores))
+            # Memperbarui skor terbaik dan cube terbaik
+            if min_score < self.best_value:
+                self.best_value = min_score
+                self.best_cube = deepcopy(population[scores.index(self.best_value)])
+
+            # Menyimpan skor minimum dan rata-rata
+            min_scores.append(self.best_value)
             avg_scores.append(sum(scores) / len(scores))
 
-            # Memperbarui nilai objektif terbaik dan kubus terbaik
-            if (min(scores) < self.best_value):
-                self.best_value = min(scores)
-                self.best_cube = population[scores.index(self.best_value)]
-
-            # Menampilkan nilai objektif terbaik setiap iterasi
+            # Menampilkan informasi iterasi
             self.display_text = Visual.display_iteration(self.display_text, iteration, self.best_value, "genetic")
 
-        # Mengembalikan kubus terbaik, nilai objektif terbaik, populasi, nilai objektif terendah, dan nilai objektif rata-rata
+        # Mengembalikan kubus terbaik, nilai objektif terbaik, populasi, skor minimum, dan skor rata-rata
         return self.best_cube, self.best_value, population, min_scores, avg_scores
     '''-------------------- Fungsi Utama Genetic Algorithm --------------------'''
-
-        
